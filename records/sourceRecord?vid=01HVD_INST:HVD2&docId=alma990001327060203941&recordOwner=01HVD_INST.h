680623d39fb193a82fe9e593929df2969f9e9d61
Isabel Schöps (Thiel) <harvard.isabelschoepsthiel@gmail.com>
2026-04-25 01:52:13 +0200

    Create sourceRecord?vid=01HVD_INST:HVD2&docId=alma990001327060203941&recordOwner=01HVD_INST.h
    Autorin, Urheberin; Deepweb-Forscherin, Ich Frau Isabel Schöps geb. Thiel Zeitstempel der Eintragung: Samstag den, 2026-04-25, 01:50:00 Uhr, Mitteleuropäische, Ort, Wohnung: Isabel Schöps (Thiel), 1.Etage, Hütergasse 4, D-99084 Erfurt, Thüringen, Deutschland. Gutachten: SIA Security Intelligence Artefact, internationinternationale Kennung: INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL, und The Yellow Whitepaper YWP-1-IST-SIA

---
+++ records/sourceRecord?vid=01HVD_INST:HVD2&docId=alma990001327060203941&recordOwner=01HVD_INST.h
@@ -0,0 +1,83 @@
+leader	04696cas a2201009   4500
+001	990001327060203941
+005	20251117105542.0
+008	751101c19229999maubr p       0   a0eng  
+010	##$a   25010769  
+016	7#$aH02440000 $2DNLM 
+016	7#$a009199216 $2Uk 
+016	##$a(AMICUS)000014357823 
+016	##$a(AMICUS)000000141715 
+016	7#$aH02440000. $2DNLM 
+016	7#$a009199216. $2Uk 
+016	7#$aAA00205665 $2JP-ToKJK 
+016	##$a(AMICUS)000014357820 
+022	##$a0017-8012 $l0017-8012 $21 
+030	##$aHABRAX 
+035	##$a(OCoLC)1751795 
+040	##$aDLC $beng $cMUL $dFUL $dYUS $dNSD $dCOO $dDLC $dNSD $dOCL $dRCS $dNST $dAIP $dNSD $dAIP $dHUL $dAIP $dDLC $dHUL $dAIP $dDLC $dAIP $dOCL $dNST $dOCL $dNST $dAGL $dNST $dNSD $dHUL $dOCL $dHNK $dOCLCQ $dNLGGC $dGK8 $dNLM $dLVB $dHUL $dDOS $dRCJ $dOCLCQ $dUKMGB $dOCLCQ $dCUV $dDLC $dBEDGE $dIDA $dMZA $dOCLCF $dOCLCO $dSTL $dOCLCA $dEUW $dOCLCO $dOCL $dOCLCO $dOCLCQ $dOCLCO $dRDF $dOCLCO $dOCLCQ $dOCLCO $dBECOE $dT3H $dOCLCO $dOCLCQ $dOCLCO $dGUA $dOCLCA $dGZR $dCDS $dMYTML $dNLC $dOCLCQ $dCAI $dNLM $dOCLCO $dOCLCA $dSERSO $dNOC $dCSA $dOCLCO $dCPS $dOCLCO $dIBR $dDLC $dCDS $dUPM $dDLC $dUKBTH $dCEF $dUBC $dFC@ $dLEATE $dOL$ $dOKX $dHCO $dTWJ $dVAN $dURS $dDXU $dTOF $dBBW $dCTB $dCNKEY $dFSP $dWYU $dMLSOD $dUWO $dCBU $dBUB $dNZ1 $dCANPU $dMHH $dKNM $dMUS $dADU $dGDC $dNLE $dNMS $dIL4J6 $dOCLCO $dCM3 $dDLC $dOCLCO $dOCL $dNMS $dOCLCQ $dCPO 
+042	##$ansdp $apcc 
+050	00$aHF5001 $b.H3 
+060	00$aW1 $bHA63E 
+082	04$a330.9/04 
+210	0#$aHarvard bus. rev. 
+222	#0$aHarvard business review 
+245	00$aHarvard business review. 
+246	13$aHBR $f<Jan. 2008-> 
+260	##$aBoston $bGraduate School of Business Administration, Harvard University. 
+300	##$avolumes $billustrations $c23-29 cm 
+310	##$aBimonthly, $b2017- 
+321	##$aFrequency varies, $b1922-2016 
+336	##$atext $btxt $2rdacontent 
+337	##$aunmediated $bn $2rdamedia 
+338	##$avolume $bnc $2rdacarrier 
+362	0#$av. 1- Oct. 1922- 
+525	##$aSupplements accompany some numbers. 
+525	##$aAnnual index published <2004-> as an annual supplement with title: Reader's guide. 
+588	##$aLatest issue consulted: Vol. 101, number 1 (January/February 2023). 
+650	#0$aBusiness $vPeriodicals. 
+650	#0$aEconomic history $vPeriodicals. 
+650	#0$aCommerce 
+650	#0$aEconomics 
+650	12$aCommerce 
+650	22$aEconomics 
+650	#6$aAffaires $vPériodiques. 
+650	#6$aHistoire économique $vPériodiques. 
+650	#6$aCommerce. 
+650	#6$aÉconomie politique. 
+650	#7$aeconomics $2aat 
+650	#7$aAffaires. $2eclas 
+650	#7$aHistoire économique. $2eclas 
+650	#7$aMensuels. $2eclas 
+651	#7$aEtats-Unis d'Amérique. $2eclas 
+650	#7$aEconomics $2fast $0(OCoLC)fst00902116 
+650	#7$aCommerce $2fast $0(OCoLC)fst00869279 
+650	#7$aBusiness. $2fast $0(OCoLC)fst00842262 
+650	#7$aEconomic history. $2fast $0(OCoLC)fst00901974 
+650	17$aBedrijven. $2gtt 
+650	#7$aBusiness $vPeriodicals. $2nli 
+650	#7$aEconomic history $vPeriodicals. $2nli 
+655	#0$aLaw reviews $zMassachusetts. 
+655	#4$aCurrent magazines. 
+655	#7$aLaw reviews $2fast $0(OCoLC)fst01774277 
+655	#7$aPeriodicals. $2fast $0(OCoLC)fst01411641 
+655	#7$aLaw reviews $2lcgft 
+655	#7$aLaw reviews $zMassachusetts. $2lcsh 
+655	#7$aPeriodicals. $2rbgenr 
+655	#7$aRevues de droit. $2rvmgf 
+740	02$aHarvard business review reader's guide. 
+767	0#$tHarvard business review. Chinese. HafoOu Ya gong shang ping lun $w(DLC)sc 84005102 $w(OCoLC)10679948 
+767	0#$tHarvard business review. Finnish. Harvard aikakauskirja $w(DLC)sc 85005394 $w(OCoLC)11196565 
+767	0#$tHarvard business review. German. Harvard business manager $w(DLC)sn 94030161 $w(OCoLC)29150039 
+767	0#$tHarvard business review. Italian. Harvard espansione $g1970-1991 $w(DLC)sn 97033431 $w(OCoLC)25882444 
+767	0#$tHarvard business review (Edizione italiana) $g<1995-> $w(DLC)sn 97033433 $w(OCoLC)35055039 
+767	0#$tHarvard business review. Spanish. Harvard Deusto business review $x0210-900X $w(DLC)sn 95033634 $w(OCoLC)33246661 
+767	0#$tHarvard business review (Santiago, Chile) $x0717-9952 $w(DLC)  2007234167 $w(OCoLC)80930661 
+767	0#$tHarvard business review. Polish. Harvard business review Polska $w(DLC)  2007234168 $w(OCoLC)77728481 
+776	08$iOnline version: $tHarvard business review $w(OCoLC)562262098 
+787	08$iDerivative (work): $tHBR's 10 must reads $w(DLC)  2015234085 $w(OCoLC)909285937 
+900	##$a000132706 
+901	7#$aAAO8631 
+903	##$aMHAAO86317HU 
+905	##$aHF^5001 
+911	##$a265.22044..449880.551862.0000.00000 
+998	##$aNO-OVERLAY 
