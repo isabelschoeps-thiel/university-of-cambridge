98b61decc6a31afe3196c8cbf1bc78ebf85509fa
Isabel Schöps (Thiel) <harvard.isabelschoepsthiel@gmail.com>
2026-04-25 02:12:49 +0200

    Create matrix_crime.h
    Autorin, Urheberin; Deepweb-Forscherin, Ich Frau Isabel Schöps geb. Thiel Zeitstempel der Eintragung: Samstag den, 2026-04-25, 02:11:00 Uhr, Mitteleuropäische, Ort, Wohnung: Isabel Schöps (Thiel), 1.Etage, Hütergasse 4, D-99084 Erfurt, Thüringen, Deutschland. Gutachten: SIA Security Intelligence Artefact, internationinternationale Kennung: INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL, und The Yellow Whitepaper YWP-1-IST-SIA

---
+++ records/matrix_crime.h
@@ -0,0 +1,30 @@
+Source record page
+leader	     cam a2200337 a 4500
+001	990129128890203941
+005	20111109172039.0
+008	110513s2011    nyu      b    001 0 eng  
+010	##$a^^2011019801 
+020	##$a9781594203138 
+020	##$a159420313X 
+035	##$a(OCoLC)707969122 $z(OCoLC)757506899 $z(OCoLC)951700329 $z(OCoLC)966568547 $z(OCoLC)967794786 $z(OCoLC)973994066 $z(OCoLC)980543791 $z(OCoLC)985288482 $z(OCoLC)988966851 $z(OCoLC)993409316 $z(OCoLC)995464632 $z(OCoLC)1002667248 $z(OCoLC)1017930540 $z(OCoLC)1022727965 $z(OCoLC)1027970727 $z(OCoLC)1029830646 $z(OCoLC)1034875360 $z(OCoLC)1039522974 $z(OCoLC)1045671152 $z(OCoLC)1050292845 $z(OCoLC)1054975061 $z(OCoLC)1083404832 $z(OCoLC)1088633604 
+040	##$aDLC $cDLC $dYDX $dBTCTA $dYDXCP $dBKL 
+042	##$apcc 
+043	##$an-us--- 
+050	00$aHV6773.2 $b.B74 2011 
+082	00$a364.16/80973 $223 
+100	1#$aBrenner, Joel 
+245	10$aAmerica the vulnerable : $binside the new threat matrix of digital espionage, crime, and warfare / $cJoel Brenner. 
+246	30$aInside the new threat matrix of digital espionage, crime, and warfare 
+260	##$aNew York : $bPenguin Press, $c2011. 
+300	##$a308 p. ; $c25 cm. 
+504	##$aIncludes bibliographical references and index. 
+505	0#$aElectronically undressed -- A primer on cyber crime -- Bleeding wealth -- Degrading defense -- Dancing in the dark -- Between war and peace -- June 2017 -- Spies in a glass house -- Thinking about intelligence -- Managing the mess. 
+520	##$aA former top-level national Security Agency insider evaluates pressing threats in digital security, revealing how operatives from hostile nations have infiltrated power, banking, and military systems to steal information and sabotage defense mechanisms. 
+650	#0$aComputer crimes $zUnited States $xPrevention 
+650	#0$aInternet in espionage $zUnited States. 
+650	#0$aNational security $zUnited States 
+900	##$a012912889 
+911	##$a415.37880..565378.651123.0000.00000 
+991	##$b2024-01-28 $cWorldCat record variable field(s) change: 758 
+991	##$b2025-11-29 $cWorldCat record variable field(s) change: 019 
+998	##$aNO-OVERLAY 
