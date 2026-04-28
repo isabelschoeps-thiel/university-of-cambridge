3a114881038dde0d15b156e11085ca546d2f9377
Isabel Schöps (Thiel) <harvard.isabelschoepsthiel@gmail.com>
2026-04-29 01:03:28 +0200

    Delete Matrix_Crime_White_House_Farm/17.6.E23.tex.h

--- Matrix_Crime_White_House_Farm/17.6.E23.tex.h
+++
@@ -1,4 +0,0 @@
-\[q\left(1-\frac{a}{c}\right){{}_{2}\phi_{1}}\left({a/q,b\atop c};q,z\right)+(1-%
-a)\left(1-\frac{abz}{c}\right){{}_{2}\phi_{1}}\left({aq,b\atop c};q,z\right)=%
-\left(1+q-a-\frac{aq}{c}+\frac{a^{2}z}{c}-\frac{abz}{c}\right){{}_{2}\phi_{1}}%
-\left({a,b\atop c};q,z\right),\]
